e665f689419a3d053ca7937317484dfcdf3e5938
OGCodee <xxgdev@gmail.com>
2020-12-20 20:04:45 +0700

    Fix For Item ( HTML, CSS & JS )

--- menu/js/header.js
+++ menu/js/header.js
@@ -2,16 +2,16 @@ document.write(`
 	<header class="carousel slide carousel-fade">
 	    <div class="carousel-inner">
 		    <div class="carousel-item active">
-		    	<img class="w-100" src="img/carousel/carousel1.jpg" alt="ROG Zephyrus Duo 15">
+		    	<img class="w-100" src="../img/carousel/carousel1.jpg" alt="ROG Zephyrus Duo 15">
 		    </div>
 		    <div class="carousel-item">
-		    	<img class="w-100" src="img/carousel/carousel2.jpg" alt="ROG Zephyrus S GX502">
+		    	<img class="w-100" src="../img/carousel/carousel2.jpg" alt="ROG Zephyrus S GX502">
 		    </div>
 		    <div class="carousel-item">
-		    	<img class="w-100" src="img/carousel/carousel3.jpg" alt="Lenovo Legion 7i">
+		    	<img class="w-100" src="../img/carousel/carousel3.jpg" alt="Lenovo Legion 7i">
 		    </div>
 		    <div class="carousel-item">
-		    	<img class="w-100" src="img/carousel/carousel4.jpg" alt="RTX 3080">
+		    	<img class="w-100" src="../img/carousel/carousel4.jpg" alt="RTX 3080">
 		    </div>
 		</div>
 		<a class="carousel-control-prev" href="header" role="button" data-slide="prev">
@@ -19,6 +19,5 @@ document.write(`
 	  	</a>
 	  	<a class="carousel-control-next" href="header" role="button" data-slide="next">
 			<span class="carousel-control-next-icon"></span>
-		</a>
-	</header>
+    </header>
 `);
---
+++ menu/js/nav-category.js
@@ -0,0 +1,62 @@
+document.write(`
+    <nav id="category" class="navbar navbar-expand-lg navbar-dark bg-black">
+	    <ul class="navbar-nav mr-auto center">
+	    	<li class="nav-item">
+	    	  	<a class="btn btn-outline-warning" href="menu/laptop.html">
+	    			<img class="category-icon" src="../img/icon/laptop.png" alt="Laptop">
+	    			<br>
+	    			Laptop
+	    		</a>
+	    	</li>
+	    	<li class="nav-item">
+	    		<a class="btn btn-outline-warning" href="menu/mb.html">
+	    			<img class="category-icon" src="../img/icon/motherboard.png" alt="Motherboard">
+	    			<br>
+	    			Motherboard
+	    		</a>
+	    	</li>
+	    	<li class="nav-item">
+	    		<a class="btn btn-outline-warning" href="menu/processor.html">
+	    			<img class="category-icon" src="../img/icon/proci.png" alt="Processor">
+	    			<br>
+	    			Processor
+	    		</a>
+	    	</li>
+	    	<li class="nav-item">
+	    		<a class="btn btn-outline-warning" href="menu/ram.html">
+	    			<img class="category-icon" src="../img/icon/ram.png" alt="RAM">
+	    			<br>
+	    			RAM
+	    		</a>
+	    	</li>
+	    	<li class="nav-item">
+	    		<a class="btn btn-outline-warning" href="menu/hdd.html">
+	    			<img class="category-icon" src="../img/icon/hdd.png" alt="HDD/SSD">
+	    			<br>
+	    			HDD/SSD
+	    		</a>
+	    	</li>
+	    	<li class="nav-item">
+	    		<a class="btn btn-outline-warning" href="menu/psu.html">
+	    			<img class="category-icon" src="../img/icon/psu.png" alt="Power Supply">
+	    			<br>
+	    			PSU
+	    		</a>
+	    	</li>
+	    	<li class="nav-item">
+	    		<a class="btn btn-outline-warning" href="menu/keyboard.html">
+	    			<img class="category-icon" src="../img/icon/keyboard.png" alt="Keyboard">
+	    		  	<br>
+	    			Keyboard
+	    		</a>
+	    	</li>
+	    	<li class="nav-item">
+	    		<a class="btn btn-outline-warning" href="menu/mouse.html">
+	    			<img class="category-icon" src="../img/icon/mouse.png" alt="Mouse">
+	    			<br>
+	    			Mouse
+	    		</a>
+	    	</li>
+	    </ul>
+    </nav>
+`);
